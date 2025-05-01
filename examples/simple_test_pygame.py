1dd349c4c0456304e76ca5c0766aade62e81b824
Bernhard Bablok <bablokb@gmx.de>
2025-05-01 14:10:12 +0200

    re-implement for newest blinka-pygame-display version

--- examples/simple_test_pygame.py
+++ examples/simple_test_pygame.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# simple_test_pygame.py
+# pygame_dev.py
 #
 # A simple test for a pygame-display simulating a 7789-display.
 #
@@ -10,6 +10,7 @@
 #
 # ----------------------------------------------------------------------------
 
+import threading
 import board
 import time
 import busio
@@ -19,73 +20,130 @@ from dataviews.Base import Color, Justify
 from dataviews.DisplayFactory import DisplayFactory
 from dataviews.DataView import DataView
 
-# always release displays (unless you use a builtin-display)
-if not hasattr(board,'DISPLAY'):
-  displayio.release_displays()
+WIDTH  = 240
+HEIGHT = 135
+AUTO_REFRESH = True
 
-# create display
-if hasattr(board,'DISPLAY'):
-  display = board.DISPLAY
-else:
-  display = DisplayFactory.pygame(width=240,height=135,native_frames_per_second=1)
+# --- various tests (returns seconds to wait after test)   -------------------
 
-# create view
-view = DataView(
-  dim=(3,2),
-  width=display.width,height=display.height,
-  col_width=[0.3,0.7],
-  justify=Justify.CENTER,
-  color=Color.GREEN,
-  fontname="fonts/DejaVuSansMono-Bold-24-subset.bdf",
-  formats=["min:","{0:.1f}mV",
-           "avg:","{0:.1f}mV",
-           "max:","{0:.1f}mV"],
-  border=1,
-  divider=1,
-  padding=5,
-)
+view = None
+tests = []
+def init():
+  """ create view """
+  global view
 
-# show without values
-display.show(view)
-time.sleep(3)
+  print("creating view")
+  view = DataView(
+    dim=(3,2),
+    width=WIDTH,height=HEIGHT,
+    col_width=[0.3,0.7],
+    justify=Justify.CENTER,
+    color=Color.GREEN,
+    fontname="fonts/DejaVuSansMono-Bold-24-subset.bdf",
+    formats=["min:","{0:.1f}mV",
+             "avg:","{0:.1f}mV",
+             "max:","{0:.1f}mV"],
+    border=1,
+    divider=1,
+    padding=5,
+  )
+  return 3
 
-# now set values
-view.set_values(
-  [None,  7.1,
-   None, 22.3,
-   None, 30.8]
-)
-time.sleep(3)
+def test1():
+  """ show initial values """
+  global view
+  print("show initial values")
+  view.set_values(
+    [None,  7.1,
+     None, 22.3,
+     None, 30.8]
+  )
+  return 3
+tests.append(test1)
 
-# flip background/forground
-view.invert()
-time.sleep(3)
-view.invert()
-time.sleep(3)
+def test2():
+  """ invert """
+  global view
+  print("invert display")
+  view.invert()
+  return 3
+tests.append(test2)
+tests.append(test2)
 
-# realign all fields
-view.justify(Justify.RIGHT)
-time.sleep(3)
+def test3():
+  """ justify right """
+  global view
+  print("justify right")
+  view.justify(Justify.RIGHT)
+  return 3
+tests.append(test3)
 
-# realign only fields in first column
-for index in [0,2,4]:
-  view.justify(Justify.LEFT,index)
-  view.set_color(Color.BLUE,index)
-  time.sleep(1)
+def test4():
+  """ realign first column """
+  global view
+  for index in [0,2,4]:
+    print(f"realign column {index}")
+    view.justify(Justify.LEFT,index)
+    view.set_color(Color.BLUE,index)
+  return 1
+tests.append(test4)
 
-# set dynamic colors
-for index in [1,3,5]:
-  view.set_color(index=index,
-                 color_range=[(Color.BLUE,15),
-                              (Color.WHITE,24),(Color.RED,None)])
-time.sleep(3)
+def test5():
+  """ set dynamic colors """
+  global view
+  for index in [1,3,5]:
+    print(f"set color for column {index}")
+    view.set_color(index=index,
+                   color_range=[(Color.BLUE,15),
+                                (Color.WHITE,24),(Color.RED,None)])
+  return 0
+tests.append(test5)
+
+# --- update values   --------------------------------------------------------
 
 i = 0
-def on_time():
-  global i
+def update_values():
+  global i, view
   view.set_values([None,i,
                    None,2.0*i/3.0,
                    None,30-i])
+  print("updating values")
   i = (i+1)%30
+  return 1
 
-display.event_loop(interval=1,on_time=on_time)
+# --- main program   --------------------------------------------------------- 
+
+# Make the display context
+main_group = displayio.Group()
+init()
+main_group.append(view)
+display = DisplayFactory.pygame(width=WIDTH,height=HEIGHT,
+                                native_frames_per_second=10,
+                                auto_refresh=AUTO_REFRESH)
+display.root_group = main_group
+
+finished = False
+while not finished:
+  print("------------------")
+  for test in tests:
+    wait = test()
+    if not AUTO_REFRESH:
+      display.refresh()
+    print("------------------")
+    end = time.monotonic() + wait
+    while time.monotonic() < end:
+      if display.check_quit():
+        finished = True
+        break
+    if finished:
+      break
+
+  while not finished:
+    wait = update_values()
+    if not AUTO_REFRESH:
+      display.refresh()
+    end = time.monotonic() + wait
+    while time.monotonic() < end:
+      if display.check_quit():
+        finished = True
+        break
